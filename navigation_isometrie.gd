a44f1c9c16bd9a3eeb14d7df99bfba561c5d2b08
Marwan Sabih <marwansabih@gmx.de>
2024-07-27 09:17:20 +0200

    runs faster now

--- navigation_isometrie.gd
+++ navigation_isometrie.gd
@@ -13,68 +13,12 @@ var edges = []
 
 var occupied_polygons = []
 
-func pad_mesh(mesh, value):
-	var padded_mesh = []
-	for i in range(dim_x +2):
-		var line = []
-		for j in range(dim_y +2):
-			line.append(value)
-		padded_mesh.append(line)
-		
-	for i in range(dim_x):
-		for j in range(dim_y):
-			padded_mesh[i+1][j+1] = mesh[i][j]
 	
-	return padded_mesh
-	
-func occupied_neigbour_exist(i,j, mesh):
-	pass
-	
-func generate_shapes():
-	"""
-	Used to generate the shapes representing...
-	maybe not needed
-	"""
-	var p_occupancy_mesh = pad_mesh(occupancy_mesh, false)
-	for i in range(dim_x+1):
-		for j in range(dim_y+1):
-			pass
-			
-		
 func outside_region(point):
 	for polygon in $Poly.get_children():
 		if Geometry2D.is_point_in_polygon(point, polygon.polygon):
 			return true
 	return false
-
-func mark_specific_corner(corner_check):
-	for ele in corner_check:
-		if ele[0] < 0 or ele[1] < 0:
-			return
-		if ele[0] == dim_x or ele[1] == dim_y:
-			return
-		if occupancy_mesh[ele[0]][ele[1]]:
-			return
-	var c = corner_check[0]
-	edge_mesh[c[0]][c[1]] = true
-	edges.append(base_mesh[c[0]][c[1]])
-	
-func mark_corner(i, j):
-	var upper_left = [[i-1, j-1], [i, j-1], [i-1, j]]
-	var upper_right = [[i+1, j-1], [i+1,j], [i, j-1]]
-	var lower_left = [[i-1, j+1], [i, j+1], [i-1, j]]
-	var lower_right = [[i+1, j+1], [i, j+1], [i+1, j]]
-	mark_specific_corner(upper_left)
-	mark_specific_corner(upper_right)
-	mark_specific_corner(lower_left)
-	mark_specific_corner(lower_right)
-	
-
-func mark_corners():
-	for i in range(dim_x):
-		for j in range(dim_x):
-			if occupancy_mesh[i][j]:
-				mark_corner(i,j)
 
 func get_edge_point(polygon, p_point, point, n_point):
 	var dir_1 = (p_point - point).normalized()
@@ -104,75 +48,7 @@ func generate_edges():
 	for polygon in $Poly.get_children():
 		edges.append_array(generate_polygon_edges(polygon))
 	return edges
-
-func get_neighbours(i,j, visited):
-	var neighbours = []
-	if i != 0 and not visited[i-1][j] and occupancy_mesh[i-1][j]:	
-		neighbours.append([i-1,j])
-		visited[i-1][j] = true
-	if not i+1 == dim_x and not visited[i+1][j] and occupancy_mesh[i+1][j]:
-		neighbours.append([i+1,j])
-		visited[i+1][j] = true
-	if not j+1 == dim_y and not visited[i][j+1] and occupancy_mesh[i][j+1]:
-		neighbours.append([i,j+1])
-		visited[i][j+1] = true
-	if not visited[i][j-1] and occupancy_mesh[i][j-1]:
-		neighbours.append([i,j-1])
-		visited[i][j-1] = true
-	return neighbours
 		
-
-func create_shape(i,j, visited):
-	var shape = []
-	var neighbours = [[i,j]]
-	while neighbours:
-		var pos = neighbours.pop_front()
-		shape.append(pos)
-		var k = pos[0]
-		var l = pos[1]
-		visited[k][l] = true
-		var neigh = get_neighbours(k, l, visited)
-		neighbours.append_array(neigh)
-	return shape
-	
-	
-func create_shapes():
-	var visited = []
-	for i in range(dim_x):
-		var line = []
-		for j in range(dim_y):
-			line.append(false)
-		visited.append(line)
-		
-	var found_shapes = []
-		
-	for i in range(dim_x):
-		for j in range(dim_y):
-			if visited[i][j]:
-				continue
-			if not occupancy_mesh[i][j]:
-				continue
-			var shape_raw = create_shape(i, j, visited)
-			shape_raw.sort()
-			var shape = []
-			var start = null
-			var last = null
-			for ele in shape_raw:
-				if not start:
-					start = ele[0]
-					shape.append(ele)
-					continue
-				if start == ele[0]:
-					last = ele
-				else:
-					if not last in shape:
-						shape.append(last)
-					shape.append(ele)
-					start = ele[0]
-			shape.append(last) 
-			found_shapes.append(shape)
-			
-	return found_shapes
 	
 			
 		
@@ -189,6 +65,27 @@ var graph
 var position_to_units = {}
 var position_to_visible_edges = {}
 
+func inside_polygons(p, polygons):
+	for polygon in polygons:
+		if Geometry2D.is_point_in_polygon(p, polygon):
+			return true
+	return false
+
+func generate_shadow_polygons(point):
+	var shadow_polygons = []
+	for polygon in occupied_polygons:
+		var size = len(polygon)
+		var shadow_polygon = []
+		for i in range(size):
+			var next_i = (i + 1) % size
+			var p = polygon[i]
+			var q = polygon[next_i]
+			var r = q + (q-point).normalized() * 10000
+			var s = p + (p-point).normalized() * 10000
+			shadow_polygons.append([p, q, r, s])
+	return shadow_polygons
+			
+
 func add_visible_edges(point):
 	var visible_edges = []
 	position_to_visible_edges[point] = []
@@ -197,12 +94,27 @@ func add_visible_edges(point):
 		if not intersect_with_occupied_polygons(point, edges[i]):
 			position_to_visible_edges[point].append(i)
 
+func generate_position_to_visible_edges_2():
+	var shadow_polygons = []
+	for edge in edges:
+		var polys = generate_shadow_polygons(edge)
+		shadow_polygons.append(polys)
+		
+	for i in range(dim_x):
+		for j in range(dim_y):
+			var visible_edges = []
+			var pos = Vector2(i*8, j*8)
+			for k in range(len(edges)):
+				if not inside_polygons(pos, shadow_polygons[k]):
+					visible_edges.append(k)
+			position_to_visible_edges[pos] = visible_edges
+			
+			
 func generate_position_to_visible_edges():
 	for i in range(dim_x):
 		for j in range(dim_y):
 			var pos = Vector2(i*8, j*8)
 			add_visible_edges(pos)	
-	
 
 func shortest_path_between_positions(
 	p1: Vector2,
@@ -267,12 +179,11 @@ func _ready():
 		
 	graph = Dijstra.generate_graph(edges, connections)
 	generate_dists(graph)
-	generate_position_to_visible_edges()
+	generate_position_to_visible_edges_2()
 	shortest_path = shortest_path_between_positions(
 		Vector2(64, 64),
 		Vector2(800, 400)
 	)
-	#shortest_path = Dijstra.find_shortest_path(graph, graph[0], graph[7])
 	
 	
 	
@@ -292,23 +203,4 @@ func _draw():
 		
 	for e in v_e_2:
 		draw_circle(edges[e], 4, Color.RED)
-	
-	"""
-	
-	Vector2(64, 64),
-	Vector2(256, 320)
-	
-	for i in dim_x:
-		for j in dim_y:
-			var point = base_mesh[i][j]
-			if edge_mesh[i][j]:
-				draw_circle(point, 4, Color.BLUE)
-			else:
-				draw_circle(point, 2, Color.BLACK)
-						
-	for i in range(len(shortest_path) - 1):
-		var start = shortest_path[i]
-		var end = shortest_path[i+1]
-		draw_line(start, end, Color.RED)
-	"""
 	
